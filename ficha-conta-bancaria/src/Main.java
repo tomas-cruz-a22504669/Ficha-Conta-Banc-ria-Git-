22f3d3c710ea408d1013ac632247f10dc2cd2d61
tomas-cruz-a22504669 <eliteopooo@gmail.com>
2025-10-15 11:31:00 +0100

    Delete ficha-conta-bancaria directory

--- ficha-conta-bancaria/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        ContaBancaria conta = new ContaBancaria(100);
-        conta.deposita(50);
-        conta.levanta(70);
-        conta.getSaldoComoString();
-        System.out.println(conta.getSaldoComoString());
-    }
-}
